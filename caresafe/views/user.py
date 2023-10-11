b88d64860e59f9e212bd731ff629ad1b4eb58a0d
Ryan Murphy <57873842+fork-bombed@users.noreply.github.com>
2023-10-11 18:50:14 +0100

    Refucked and unfucked the stuff

--- caresafe/views/user.py
+++ caresafe/views/user.py
@@ -19,6 +19,10 @@ def user_home(user_id):
     )
 
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> 526ed0b (Refucked and unfucked the stuff)
 @bp.route('/panic', methods=['POST'])
 @require_auth
 def user_panic(user_id):
@@ -27,6 +31,45 @@ def user_panic(user_id):
     panic.save()
     
 
+<<<<<<< HEAD
+=======
+ 
+@bp.route('/extend', methods=['POST'])
+@require_auth
+def extend_session(user_id):
+
+    try:
+        data = request.get_json()
+        extension = data.get('extension_time')
+        appointment_id = data.get('appointment_id')
+
+        user = User.query.get(user_id)
+        appointment = Appointment.query.get(appointment_id)
+
+        if user is None:
+            return jsonify({'message': 'User not found'}), 404
+
+        if appointment is None:
+            return jsonify({'message': 'Appointment not found'}), 404
+
+        if user.id != appointment.user.id:
+            return jsonify({'message': 'Permission denied'}), 403
+
+        appointment.duration += int(extension)
+        db.session.commit()  # Commit changes to the database
+
+        return jsonify({'message': 'Appointment extended'}), 200
+
+    except (ValueError, IntegrityError) as e:
+        db.session.rollback()  # Roll back changes on error
+        return jsonify({'message': 'Invalid data or database error'}), 400
+
+    except Exception as e:
+        db.session.rollback()  # Roll back changes on error
+        return jsonify({'message': f'Failed to update: {str(e)}'}), 400
+    
+
+>>>>>>> 526ed0b (Refucked and unfucked the stuff)
 @bp.route('/appointments', methods=['GET'])
 @require_auth
 def get_user_appointments(user_id):
