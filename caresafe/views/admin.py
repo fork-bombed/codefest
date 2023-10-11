8423917e897b2616e10deef33d9356bf1f772ce2
Ryan Murphy <57873842+fork-bombed@users.noreply.github.com>
2023-10-11 16:47:23 +0100

    Fix auth for admin

--- caresafe/views/admin.py
+++ caresafe/views/admin.py
@@ -6,5 +6,5 @@ bp = Blueprint('admin', __name__, url_prefix='/admin')
 @bp.route('/')
 @require_auth
 @require_admin
-def admin_home():
+def admin_home(user_id):
     return jsonify({'message': 'Welcome to the admin page!'})
