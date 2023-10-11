40bed54a7643065b37913cb5af069f4f507519ab
Ryan Murphy <57873842+fork-bombed@users.noreply.github.com>
2023-10-11 19:07:59 +0100

    Fix smol issue

--- caresafe/views/user.py
+++ caresafe/views/user.py
@@ -19,48 +19,12 @@ def user_home(user_id):
     )
 
 
-
 @bp.route('/panic', methods=['POST'])
 @require_auth
 def user_panic(user_id):
     appointment_id = request.json.get('appointment_id')
     panic = Panic(appointment_id=appointment_id, user_id=user_id)
     panic.save()
-
- 
-@bp.route('/extend', methods=['POST'])
-@require_auth
-def extend_session(user_id):
-
-    try:
-        data = request.get_json()
-        extension = data.get('extension_time')
-        appointment_id = data.get('appointment_id')
-
-        user = User.query.get(user_id)
-        appointment = Appointment.query.get(appointment_id)
-
-        if user is None:
-            return jsonify({'message': 'User not found'}), 404
-
-        if appointment is None:
-            return jsonify({'message': 'Appointment not found'}), 404
-
-        if user.id != appointment.user.id:
-            return jsonify({'message': 'Permission denied'}), 403
-
-        appointment.duration += int(extension)
-        db.session.commit()  # Commit changes to the database
-
-        return jsonify({'message': 'Appointment extended'}), 200
-
-    except (ValueError, IntegrityError) as e:
-        db.session.rollback()  # Roll back changes on error
-        return jsonify({'message': 'Invalid data or database error'}), 400
-
-    except Exception as e:
-        db.session.rollback()  # Roll back changes on error
-        return jsonify({'message': f'Failed to update: {str(e)}'}), 400
     
 
 @bp.route('/appointments', methods=['GET'])
@@ -70,15 +34,6 @@ def get_user_appointments(user_id):
     appointments = user.appointments
     return jsonify(
         {
-            'appointments': [
-                {
-                    'id': appointment.id,
-                    'time': appointment.time.strftime('%H:%M'),
-                    'duration': appointment.duration,
-                    'client_id': appointment.client_id,
-                    'user_id': appointment.user_id,
-                }
-                for appointment in appointments
-            ]
+            'appointments': [appointment.as_dict() for appointment in appointments]
         }
     )
