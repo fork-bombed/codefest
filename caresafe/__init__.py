0829505a0fac55fd2259051761015b762372bd95
Chris Kane <chris@chriskane.com>
2023-10-12 00:20:18 +0100

    downgraded some bullshit versions

--- caresafe/__init__.py
+++ caresafe/__init__.py
@@ -14,6 +14,7 @@ def create_app():
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI")
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     with app.app_context():
         db.init_app(app)
         bcrypt.init_app(app)
