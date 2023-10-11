7d0c8db55fd3ecf8ee4c059fa85edcdb54b3acc1
Calum Stewart <CalumAlexanderStewart@gmail.com>
2023-10-11 19:06:39 +0100

    ui working

--- client/CareSafe/App.js
+++ client/CareSafe/App.js
@@ -1,14 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './src/screens/LoginScreen';
+import MainScreen from './src/screens/MainScreen';
 
-export default function App() {
+
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Main" component={MainScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
---
+++ client/CareSafe/src/screens/LoginScreen.js
@@ -0,0 +1,95 @@
+// LoginScreen.js
+
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Image,
+  TouchableOpacity
+} from 'react-native';
+
+const LoginScreen = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Handle your login logic here
+    console.log('Login pressed with username:', username, 'and password:', password);
+  };
+
+  return (
+    <View style={styles.container}>
+
+      <Image source={require('../../assets/CareSafe.png')} style={styles.logo} />
+
+      <Text style={styles.title}>Welcome Back!</Text>
+
+      <TextInput
+        value={username}
+        onChangeText={setUsername}
+        placeholder="Username"
+        style={styles.input}
+      />
+
+      <TextInput
+        value={password}
+        onChangeText={setPassword}
+        placeholder="Password"
+        secureTextEntry
+        style={styles.input}
+      />
+
+      <TouchableOpacity onPress={handleLogin} style={styles.loginButton}>
+        <Text style={styles.loginButtonText}>Login</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+    color: '#333',
+    fontWeight: 'bold',
+  },
+  input: {
+    width: '100%',
+    padding: 15,
+    marginBottom: 10,
+    backgroundColor: 'white',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#ddd',
+  },
+  loginButton: {
+    width: '100%',
+    padding: 15,
+    backgroundColor: '#007BFF',
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  loginButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  logo: {
+    width: 350,
+    height: 350,
+    marginBottom: 20,
+    resizeMode: 'contain',
+  },
+});
+
+export default LoginScreen;
