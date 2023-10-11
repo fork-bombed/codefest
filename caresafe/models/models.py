3ab34d2696fe4d91943580678ff54af24941c7f9
Ryan Murphy <57873842+fork-bombed@users.noreply.github.com>
2023-10-11 18:14:29 +0100

    Add appointments and clients

--- caresafe/models/models.py
+++ caresafe/models/models.py
@@ -13,6 +13,10 @@ class Client(db.Model):
 
     appointments = db.relationship('Appointment', back_populates='client')
 
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
     def __repr__(self):
         return f'<Client {self.first_name} {self.last_name}>'
 
@@ -29,6 +33,10 @@ class Appointment(db.Model):
     client = db.relationship('Client', back_populates='appointments')
     user = db.relationship('User', back_populates='appointments')  
     panics = db.relationship('Panic', back_populates='appointment')
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
 
     def __repr__(self):
         return f'<Appointment {self.id}>'
